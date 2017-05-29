bfc6087470cac4518077cd93da833518cd37f3fa
Praney Behl <praneybehl@gmail.com>
2017-05-30 06:48:18 +1000

    Add Column Component

---
+++ src/Column.svelte
@@ -0,0 +1,24 @@
+<div class="column
+	{{isThreeQuaters ? 'is-three-quarters' : ''}}
+	{{isTwoThirds ? 'is-two-thirds' : ''}}
+	{{isHalf ? 'is-half' : ''}}
+	{{isOneThird ? 'is-one-third' : ''}}
+	{{isOneQuarter ? 'is-one-quarter' : ''}}
+	{{ classes }}">
+</div>
+
+<script>
+	export default {
+		data() {
+			return {
+				classes: '',
+				//Sizes
+				isThreeQuaters: false,
+				isTwoThirds: false,
+				isHalf: false,
+				isOneThird: false,
+				isOneQuarter: false
+			}
+		}
+	};
+</script>
